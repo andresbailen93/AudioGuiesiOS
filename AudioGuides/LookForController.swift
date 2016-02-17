cbea00bd13059a408e4e46e310f5751fd00f9903
Andres Bailen Jimenez <andresbailen93@gmail.com>
2016-02-17 13:23:37 +0100

    Vistas y controladores

---
+++ AudioGuides/LookForController.swift
@@ -0,0 +1,21 @@
+//
+//  LookForController.swift
+//  AudioGuides
+//
+//  Created by Andrés Bailén Jiménez on 17/2/16.
+//  Copyright © 2016 Andrés Bailén Jiménez. All rights reserved.
+//
+
+import UIKit
+
+class LookForController: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- AudioGuides/ViewController.swift
+++ AudioGuides/ViewController.swift
@@ -9,6 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: Properties
+    
+    // MARK: Actions
+    
+    @IBAction func locateMonument(sender: UIButton) {
+    }
+    @IBAction func findMonument(sender: UIButton) {
+    }
+    @IBAction func viewHistory(sender: UIButton) {
+    }
+    @IBAction func viewInfo(sender: UIButton) {
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
